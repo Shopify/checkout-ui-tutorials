8848d5f9cef4b19d741cc59f8f8868eaf8342142
Shaina Raskas <shaina.raskas@shopify.com>
2023-05-12 09:42:42 -0400

    fix annotations

--- extensions/product-offer-pre-purchase-node/src/index.jsx
+++ extensions/product-offer-pre-purchase-node/src/index.jsx
@@ -38,25 +38,25 @@ function App() {
   const [products, setProducts] = useState([]);
 // [END product_offer-pre_purchase-react.sec_2-step_2_1]
 
-// [START product_offer-pre_purchase-react.sec_2-step_3-2]
+// [START product_offer-pre_purchase-react.sec_2-step_3_2]
   const [loading, setLoading] = useState(false);
-// [END product_offer-pre_purchase-react.sec_2-step_3-2]
+// [END product_offer-pre_purchase-react.sec_2-step_3_2]
 
-// [START product_offer-pre_purchase-react.sec_2-step_3-1]
+// [START product_offer-pre_purchase-react.sec_2-step_3_1]
   const [adding, setAdding] = useState(false);
-// [START product_offer-pre_purchase-react.sec_2-step_3-1]
+// [START product_offer-pre_purchase-react.sec_2-step_3_1]
 
-// [START product_offer-pre_purchase-react.sec_2-step_3-3]
+// [START product_offer-pre_purchase-react.sec_2-step_3_3]
   const [showError, setShowError] = useState(false);
-// [START product_offer-pre_purchase-react.sec_2-step_3-3]
+// [START product_offer-pre_purchase-react.sec_2-step_3_3]
 
 // [START product_offer-pre_purchase-react.sec_2-step_2_1]
   useEffect(() => {
 // [END product_offer-pre_purchase-react.sec_2-step_2_1]
 
-// [START product_offer-pre_purchase-react.sec_2-step_3-2]
+// [START product_offer-pre_purchase-react.sec_2-step_3_2]
     setLoading(true);
-// [END product_offer-pre_purchase-react.sec_2-step_3-2]
+// [END product_offer-pre_purchase-react.sec_2-step_3_2]
 
 // [START product_offer-pre_purchase-react.sec_2-step_2_1]
     query(
@@ -90,32 +90,32 @@ function App() {
     })
 // [END product_offer-pre_purchase-react.sec_2-step_2_1]
 
-// [START product_offer-pre_purchase-react.sec_2-step_3-3]
+// [START product_offer-pre_purchase-react.sec_2-step_3_3]
     .catch((error) => console.error(error))
-// [START product_offer-pre_purchase-react.sec_2-step_3-3]
+// [START product_offer-pre_purchase-react.sec_2-step_3_3]
 
-// [START product_offer-pre_purchase-react.sec_2-step_3-2]
+// [START product_offer-pre_purchase-react.sec_2-step_3_2]
     .finally(() => setLoading(false));
-// [END product_offer-pre_purchase-react.sec_2-step_3-2]
+// [END product_offer-pre_purchase-react.sec_2-step_3_2]
 
 // [START product_offer-pre_purchase-react.sec_2-step_2_1]
   }, []);
 // [END product_offer-pre_purchase-react.sec_2-step_2_1]
 
-// [START product_offer-pre_purchase-react.sec_2-step_3-3]
+// [START product_offer-pre_purchase-react.sec_2-step_3_3]
   useEffect(() => {
     if (showError) {
       const timer = setTimeout(() => setShowError(false), 3000);
       return () => clearTimeout(timer);
     }
   }, [showError]);
-// [END product_offer-pre_purchase-react.sec_2-step_3-3]
+// [END product_offer-pre_purchase-react.sec_2-step_3_3]
 
 // [START product_offer-pre_purchase-react.sec_2-step_2_2]
   const lines = useCartLines();
 // [END product_offer-pre_purchase-react.sec_2-step_2_2]
 
-// [START product_offer-pre_purchase-react.sec_2-step_3-2]
+// [START product_offer-pre_purchase-react.sec_2-step_3_2]
   if (loading) {
     return (
       <BlockStack spacing="loose">
@@ -140,13 +140,13 @@ function App() {
       </BlockStack>
     );
   }
-// [END product_offer-pre_purchase-react.sec_2-step_3-2]
+// [END product_offer-pre_purchase-react.sec_2-step_3_2]
 
-// [START product_offer-pre_purchase-react.sec_2-step_2-3]
+// [START product_offer-pre_purchase-react.sec_2-step_2_3]
   if (!loading && products.length === 0) {
     return null;
   }
-// [END product_offer-pre_purchase-react.sec_2-step_2-3]
+// [END product_offer-pre_purchase-react.sec_2-step_2_3]
 
 // [START product_offer-pre_purchase-react.sec_2-step_2_3]
   const cartLineProductVariantIds = lines.map((item) => item.merchandise.id);
@@ -173,7 +173,7 @@ function App() {
     ?? "https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_medium.png?format=webp&v=1530129081";
 // [END product_offer-pre_purchase-react.sec_2-step_2_1]
 
-// [START product_offer-pre_purchase-react.sec_2-step_3-1]
+// [START product_offer-pre_purchase-react.sec_2-step_3_1]
   return (
     <BlockStack spacing="loose">
       <Divider />
@@ -220,20 +220,20 @@ function App() {
           </Button>
         </InlineLayout>
       </BlockStack>
-// [END product_offer-pre_purchase-react.sec_2-step_3-1]
+// [END product_offer-pre_purchase-react.sec_2-step_3_1]
 
-// [START product_offer-pre_purchase-react.sec_2-step_3-3]
+// [START product_offer-pre_purchase-react.sec_2-step_3_3]
       {showError && (
         <Banner status="critical">
           There was an issue adding this product. Please try again.
         </Banner>
       )}
-// [END product_offer-pre_purchase-react.sec_2-step_3-3]
+// [END product_offer-pre_purchase-react.sec_2-step_3_3]
 
-// [START product_offer-pre_purchase-react.sec_2-step_3-1]
+// [START product_offer-pre_purchase-react.sec_2-step_3_1]
       </BlockStack>
   );
-// [END product_offer-pre_purchase-react.sec_2-step_3-1]
+// [END product_offer-pre_purchase-react.sec_2-step_3_1]
 
 // [START product_offer-pre_purchase-react.sec_2-step_2_1]
 }
