275649d2a4403bef1429fe12f735b1febe223a05
Shaina Raskas <shaina.raskas@shopify.com>
2023-05-12 15:15:37 -0400

    work around nesting issues

--- extensions/product-offer-pre-purchase-node/src/index.jsx
+++ extensions/product-offer-pre-purchase-node/src/index.jsx
@@ -23,6 +23,7 @@ import {
 // [START product_offer-pre_purchase-react.sec_2-step_1]
 render("Checkout::Dynamic::Render", () => <App />);
 // [END product_offer-pre_purchase-react.sec_2-step_1]
+
 // [START product_offer-pre_purchase-react.sec_2-step_2_1]
 function App() {
   const { query, i18n } = useExtensionApi();
@@ -80,6 +81,7 @@ function App() {
 // [END product_offer-pre_purchase-react.sec_2-step_3_2]
   }, []);
 // [END product_offer-pre_purchase-react.sec_2-step_2_1]
+
 // [START product_offer-pre_purchase-react.sec_2-step_3_3]
   useEffect(() => {
     if (showError) {
@@ -88,9 +90,11 @@ function App() {
     }
   }, [showError]);
 // [END product_offer-pre_purchase-react.sec_2-step_3_3]
+
 // [START product_offer-pre_purchase-react.sec_2-step_2_2]
   const lines = useCartLines();
 // [END product_offer-pre_purchase-react.sec_2-step_2_2]
+
 // [START product_offer-pre_purchase-react.sec_2-step_3_2]
   if (loading) {
     return (
@@ -135,11 +139,13 @@ function App() {
     return null;
   }
 // [END product_offer-pre_purchase-react.sec_2-step_2_3]
+
 // [START product_offer-pre_purchase-react.sec_2-step_2_1]
   const { images, title, variants } = productsOnOffer[0];
   const renderPrice = i18n.formatCurrency(variants.nodes[0].price.amount);
   const imageUrl = images.nodes[0]?.url
     ?? "https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_medium.png?format=webp&v=1530129081";
+// [END product_offer-pre_purchase-react.sec_2-step_2_1]
 // [START product_offer-pre_purchase-react.sec_2-step_3_1]
   return (
     <BlockStack spacing="loose">
@@ -197,5 +203,6 @@ function App() {
       </BlockStack>
   );
 // [END product_offer-pre_purchase-react.sec_2-step_3_1]
+// [START product_offer-pre_purchase-react.sec_2-step_2_1]
 }
 // [END product_offer-pre_purchase-react.sec_2-step_2_1]
