05efb58616eaf060d42dd8e22ddedc2ef8596f2d
Shaina Raskas <shaina.raskas@shopify.com>
2023-05-12 15:38:11 -0400

    kill more spaces

--- extensions/product-offer-pre-purchase-node/src/index.jsx
+++ extensions/product-offer-pre-purchase-node/src/index.jsx
@@ -128,13 +128,11 @@ function App() {
     );
   }
 // [END product_offer-pre_purchase-react.sec_2-step_3_2]
-
 // [START product_offer-pre_purchase-react.sec_2-step_2_3]
   if (!loading && products.length === 0) {
     return null;
   }
 // [END product_offer-pre_purchase-react.sec_2-step_2_3]
-
 // [START product_offer-pre_purchase-react.sec_2-step_2_3]
   const cartLineProductVariantIds = lines.map((item) => item.merchandise.id);
   const productsOnOffer = products.filter(
@@ -156,7 +154,6 @@ function App() {
   const imageUrl = images.nodes[0]?.url
     ?? "https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_medium.png?format=webp&v=1530129081";
 // [END product_offer-pre_purchase-react.sec_2-step_2_1]
-
 // [START product_offer-pre_purchase-react.sec_2-step_3_1]
   return (
     <BlockStack spacing="loose">
